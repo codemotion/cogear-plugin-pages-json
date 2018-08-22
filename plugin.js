64cc9db97ec48b7f590693689c1bc2ee51bf964c
Dmitriy Belyaev <admin@codemotion.ru>
2018-08-22 20:24:33 +0300

    Make it compatible with v1.1

--- plugin.js
+++ plugin.js
@@ -3,17 +3,17 @@ stringify = require("json-stringify-pretty-compact"),
 fs = require("fs");
 
 module.exports = {
-		apply(cogear){
+		apply(){
 			let pagesJSON = path.join(cogear.options.src, "pages.json")
 			// Process hooks here
-			cogear.hooks.build.tap('Build pages.json',()=>{
+			cogear.on('build.done',()=>{
 				// Write pages to JSON
 				fs.writeFileSync(pagesJSON,stringify(cogear.pages));
 			})
-			cogear.hooks.clearBuild.tap('Remove pages.json on clear build',()=>{
+			cogear.on('clear',()=>{
 				del.sync(pagesJSON)
 			})
-			cogear.hooks.death.tap('Remove pages.json on exit',()=>{
+			cogear.on('death',()=>{
 				del.sync(pagesJSON)
 			})
 		}
