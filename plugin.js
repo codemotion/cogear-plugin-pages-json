ec2ea4e3d6d22053ae33d03a221baa00e9eaf2e8
Dmitriy Beliaev <admin@cogear.ru>
2018-09-04 13:17:34 +0300

    Linting

--- plugin.js
+++ plugin.js
@@ -1,31 +1,31 @@
-const path = require("path");
-const stringify = require("json-stringify-pretty-compact");
-const fs = require("fs");
-const del = require("del");
+const path = require('path');
+const stringify = require('json-stringify-pretty-compact');
+const fs = require('fs');
+const del = require('del');
 
 module.exports = {
-		apply(){
-			// Process hooks here
-			cogear.on('build.done',()=>{
-				// Write pages to JSON
-				let pages = {}
-				Object.entries(cogear.pages).map(([file,page])=>{
-					pages[page.path.replace('index.html','').replace(/^/,'/')] = {
-						title: page.title,
-						content: page.content,
-					}
-				})
-				let pagesJSON = path.join(cogear.options.output, "pages.json")
-				fs.writeFileSync(pagesJSON,stringify(pages));
-				cogear.emit('pages.json',pagesJSON)
-			})
-			cogear.on('clear',()=>{
-				let pagesJSON = path.join(cogear.options.output, "pages.json")
-				del.sync(pagesJSON)
-			})
-			cogear.on('death',()=>{
-				let pagesJSON = path.join(cogear.options.output, "pages.json")
-				del.sync(pagesJSON)
-			})
-		}
-}
+  apply(){
+    // Process hooks here
+    cogear.on('build.done',()=>{
+      // Write pages to JSON
+      let pages = {};
+      Object.entries(cogear.pages).map(([file,page])=>{
+        pages[page.path.replace('index.html','').replace(/^/,'/')] = {
+          title: page.title,
+          content: page.content,
+        };
+      });
+      let pagesJSON = path.join(cogear.options.output, 'pages.json');
+      fs.writeFileSync(pagesJSON,stringify(pages));
+      cogear.emit('pages.json',pagesJSON);
+    });
+    cogear.on('clear',()=>{
+      let pagesJSON = path.join(cogear.options.output, 'pages.json');
+      del.sync(pagesJSON);
+    });
+    cogear.on('death',()=>{
+      let pagesJSON = path.join(cogear.options.output, 'pages.json');
+      del.sync(pagesJSON);
+    });
+  }
+};
